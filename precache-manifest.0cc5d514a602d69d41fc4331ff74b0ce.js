78747958e71211272e4d13fb9226ec45649243e3
Steven Enamakel <enamakel@vezures.com>
2020-09-02 01:11:02 +0530

    Updates

---
+++ precache-manifest.0cc5d514a602d69d41fc4331ff74b0ce.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f3640455f23eadfccc6ccfca970fa05",
+    "url": "/index.html"
+  },
+  {
+    "revision": "546ecdd649fe96af669d",
+    "url": "/static/css/2.1269d6b6.chunk.css"
+  },
+  {
+    "revision": "8c87b8a9964b1e2f876f",
+    "url": "/static/css/3.d50cef2a.chunk.css"
+  },
+  {
+    "revision": "546ecdd649fe96af669d",
+    "url": "/static/js/2.cbcea253.chunk.js"
+  },
+  {
+    "revision": "34edf7b1b73f86815bc487f5e9c1cbda",
+    "url": "/static/js/2.cbcea253.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8c87b8a9964b1e2f876f",
+    "url": "/static/js/3.8ef62518.chunk.js"
+  },
+  {
+    "revision": "7c93a32d8c7349c281339bf314cfe987",
+    "url": "/static/js/3.8ef62518.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3a9a55f7efd181bf86cb",
+    "url": "/static/js/4.559796a6.chunk.js"
+  },
+  {
+    "revision": "475e9ba2fbe98c2ab0a5",
+    "url": "/static/js/5.7a8d87ab.chunk.js"
+  },
+  {
+    "revision": "4f6067b7656ff922d893",
+    "url": "/static/js/main.a8f391c5.chunk.js"
+  },
+  {
+    "revision": "dffe39b9dc6d565de4e6",
+    "url": "/static/js/runtime-main.506d99b6.js"
+  },
+  {
+    "revision": "2850e915efd78232122f3e1b746f8d39",
+    "url": "/static/media/arrow.2850e915.svg"
+  },
+  {
+    "revision": "c8618c54d67488f5db37e0144cd2d838",
+    "url": "/static/media/arrowTopRight.c8618c54.svg"
+  },
+  {
+    "revision": "239a1795ce78e464bda52abf53c466d6",
+    "url": "/static/media/clock-small-grey.239a1795.svg"
+  },
+  {
+    "revision": "481e7c079a1c21a29008e8d279e67b5b",
+    "url": "/static/media/clock.481e7c07.svg"
+  },
+  {
+    "revision": "c1dddb8c5554ce9076464694fe0eae97",
+    "url": "/static/media/close-inline.c1dddb8c.svg"
+  },
+  {
+    "revision": "13b7f5111726d94892bd9ad6b0fcbb4f",
+    "url": "/static/media/copy.13b7f511.svg"
+  },
+  {
+    "revision": "e7fde7cd95a01167830b7a65f625d400",
+    "url": "/static/media/descend.e7fde7cd.svg"
+  },
+  {
+    "revision": "8e492979243974804f38dce6e09b5ef9",
+    "url": "/static/media/eth.8e492979.svg"
+  },
+  {
+    "revision": "f47656c6eff801f2a1477124e2dbbacd",
+    "url": "/static/media/external.f47656c6.svg"
+  },
+  {
+    "revision": "08a99ba7a6ec8e19eab346d0ca69f789",
+    "url": "/static/media/flag.08a99ba7.svg"
+  },
+  {
+    "revision": "f082210e172a6cdcab8f43931e288b43",
+    "url": "/static/media/ledger-logomark.f082210e.svg"
+  },
+  {
+    "revision": "878e2d8eb3759f39bc94baafe8701b1c",
+    "url": "/static/media/ledger.878e2d8e.svg"
+  },
+  {
+    "revision": "2f8ed015a9000db41f23d69243e1fe62",
+    "url": "/static/media/link-lock.2f8ed015.svg"
+  },
+  {
+    "revision": "f9ce09968f9d425070e0f5ead374be77",
+    "url": "/static/media/link.f9ce0996.svg"
+  },
+  {
+    "revision": "6f1e9d014df1185a2a0e4a4ec1b40d65",
+    "url": "/static/media/linked-wallet.6f1e9d01.svg"
+  },
+  {
+    "revision": "7d89e03231e247a64b5554a39b8af991",
+    "url": "/static/media/lock.7d89e032.svg"
+  },
+  {
+    "revision": "d0ab983a1f2e8be772297739be2bc5a9",
+    "url": "/static/media/maker-logomark.d0ab983a.svg"
+  },
+  {
+    "revision": "51352aa77d9fe0a6e3498128380b3c5e",
+    "url": "/static/media/metamask.51352aa7.svg"
+  },
+  {
+    "revision": "1b8b7f9142eb6935ba7c6f7704219d99",
+    "url": "/static/media/newtab.1b8b7f91.svg"
+  },
+  {
+    "revision": "bbfaadc1639b7005666af536ab8e2209",
+    "url": "/static/media/right-arrow.bbfaadc1.svg"
+  },
+  {
+    "revision": "c0e91112f1a78bbcd7046c71b24d551c",
+    "url": "/static/media/single-wallet.c0e91112.svg"
+  },
+  {
+    "revision": "0051139e023c6a1fb30e3115d761cd2b",
+    "url": "/static/media/time.0051139e.svg"
+  },
+  {
+    "revision": "63ee78414c4a1e1e39728ba895ddaf3c",
+    "url": "/static/media/trezor-logomark.63ee7841.svg"
+  },
+  {
+    "revision": "46a89126e25ca8eb06678d3d458f96a1",
+    "url": "/static/media/warning.46a89126.svg"
+  }
+]);
